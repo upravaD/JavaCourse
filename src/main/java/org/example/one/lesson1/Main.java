945e2eb66fd7d155298e9c26e83d228068ecb674
upravaD <daktah@icloud.com>
2023-06-23 17:42:51 +0400

    Initial commit

---
+++ src/main/java/org/example/one/lesson1/Main.java
@@ -0,0 +1,12 @@
+package org.example.one.lesson1;
+
+/**
+ Java for beginners.
+ Lesson 1: "JDK and Hello World!"
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
